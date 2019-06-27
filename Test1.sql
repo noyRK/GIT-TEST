ee9fe66a542c3f27b22d3512003992f18c7b0d4f
noyRK <rksunnynoy@gmail.com>
2019-06-27 19:52:56 +0700

    Add files via upload

---
+++ Test1.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Customer (
+	Firstame varchar(25) NOT NULL,
+	Lastname varchar(25) NOT NULL,
+	PhoneNumber varchar(15) ,
+    EmailAddress varchar(25),
+    Createdate TIMESTAMP NOT NULL
+)
+    
+
+    
